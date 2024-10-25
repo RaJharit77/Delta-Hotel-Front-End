bca49768b6518698bfa231afac7792f1d97af29f
RaJharit77 <hei.rajo.2@gmail.com>
2024-10-26 00:44:01 +0300

    feat: adding new a feature

--- src/components/Notifications.tsx
+++ src/components/Notifications.tsx
@@ -16,19 +16,19 @@ const Notifications: React.FC = () => {
 
     return (
         <div className="fixed bottom-5 right-5 bg-gray-800 bg-opacity-50 shadow-lg rounded-lg p-4 w-80 border border-gray-600 z-50">
-            <p className="text-creme font-semibold text-lg text-left">Visitez notre restaurant !</p>
+            <p className="text-creme font-semibold text-lg text-left pt-2.5">Visitez notre restaurant !</p>
             <p className="text-gray-300 mt-3 text-left">
-            Découvrez une expérience culinaire unique, alliant saveurs authentiques et convivialité, pour un moment inoubliable.
+                Découvrez une expérience culinaire unique, alliant saveurs authentiques et convivialité, pour un moment inoubliable.
             </p>
             <button
                 onClick={() => window.open("https://delta-restaurant-madagascar.vercel.app", "_blank")}
-                className="inline-block mt-5 bg-creme text-black py-2 px-4 rounded-md hover:bg-or transition duration-300 cursor-pointer"
+                className="inline-block mt-3 bg-creme text-black py-2 px-4 rounded-md hover:bg-or transition duration-300 cursor-pointer"
             >
                 En savoir plus
             </button>
             <button
                 onClick={() => setIsVisible(false)}
-                className="absolute top-2 right-2 text-or hover:text-or bg-emerald-700 bg-opacity-50 cursor-pointer"
+                className="absolute top-2 right-2 text-creme hover:text-or bg-emerald-700 bg-opacity-50 cursor-pointer"
                 aria-label="Fermer la notification"
             >
                 <FaTimes />
