f49ee89a3032331e63a2a46d2a921bc10fe3fa79
RaJharit77 <hei.rajo.2@gmail.com>
2024-10-26 14:54:55 +0300

    feat: adding new a feature

--- src/components/Notifications.tsx
+++ src/components/Notifications.tsx
@@ -15,14 +15,14 @@ const Notifications: React.FC = () => {
     if (!isVisible) return null;
 
     return (
-        <div className="fixed bottom-5 right-5 bg-gray-800 bg-opacity-50 shadow-lg rounded-lg p-4 w-80 border border-gray-600 z-50">
+        <div className="fixed bottom-5 right-5 bg-gray-800 bg-opacity-75 shadow-lg rounded-lg p-4 w-80 border border-gray-600 z-50">
             <p className="text-creme font-semibold text-lg text-left pt-2.5">Visitez notre restaurant !</p>
-            <p className="text-gray-300 mt-3 text-left">
+            <p className="text-gray-300 mt-2 text-left">
                 Découvrez une expérience culinaire unique, alliant saveurs authentiques et convivialité, pour un moment inoubliable.
             </p>
             <button
                 onClick={() => window.open("https://delta-restaurant-madagascar.vercel.app", "_blank")}
-                className="inline-block mt-3 bg-creme text-black py-2 px-4 rounded-md hover:bg-or transition duration-300 cursor-pointer"
+                className="inline-block mt-4 bg-creme text-black py-2 px-4 rounded-md hover:bg-or transition duration-300 cursor-pointer"
             >
                 En savoir plus
             </button>
